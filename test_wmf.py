a0be34b5925de983ffe86a30da0cc1b53d45a530
Sander Dieleman <sanderdieleman@gmail.com>
2014-09-03 17:06:29 -0400

    implemented WMF with biases

--- test_wmf.py
+++ test_wmf.py
@@ -7,4 +7,4 @@ B = np.load("test_matrix.pkl")
 P = wmf.binarize_matrix(B)
 S = wmf.log_surplus_confidence_matrix(B, alpha=2.0, epsilon=1e-6)
 
-U, V = wmf.factorize(P, S, num_factors=40, lambda_reg=1e-5, num_iterations=20, init_std=0.01, verbose=True, dtype='float32')
+U, V = wmf.factorize(P, S, num_factors=40, lambda_reg=1e-5, num_iterations=10, bias=True, init_std=0.01, verbose=True, dtype='float32')
--- wmf.py
+++ wmf.py
@@ -63,7 +63,58 @@ def _recompute_factors(Y, Sl, Dl, Il, lambda_reg, dtype='float32'):
 
 
 
-def factorize(P, S, num_factors, lambda_reg=1e-5, num_iterations=20, init_std=0.01, verbose=False, dtype='float32'):
+def _recompute_factors_biased(Y, Sl, Dl, Il, lambda_reg, dtype='float32'):
+    """
+    recompute matrix X from Y.
+    X = recompute_factors(Y, Sl, Dl, Il, lambda_reg_x)
+    This can also be used for the reverse operation as follows:
+    Y = recompute_factors(X, STl, DTl, ITl, lambda_reg_y)
+    
+    Sl: data for S matrix
+    Dl: data for D matrix
+    Il: indices for both matrices
+    
+    The comments are in terms of X being the users and Y being the items.
+    """
+    m = Dl.shape[0] # m = number of users
+    f = Y.shape[1] - 1 # f = number of factors
+    
+    b_y = Y[:, f] # vector of biases
+
+    Y_e = Y.copy()
+    Y_e[:, f] = 1 # factors with added column of ones
+
+    YTY = np.dot(Y_e.T, Y_e) # precompute this
+
+    R = np.eye(f + 1) # regularization matrix
+    R[f, f] = 0 # don't regularize the biases!
+    R *= lambda_reg
+
+    YTYpR = YTY + R
+
+    byY = np.dot(b_y, Y_e) # precompute this as well
+
+    X_new = np.zeros((m, f + 1), dtype=dtype)
+
+    for k, s_u, d_u, i_u in itertools.izip(xrange(m), Sl, Dl, Il):
+        Y_u = Y_e[i_u] # exploit sparsity
+        b_y_u = b_y[i_u]
+        A = d_u.dot(Y_u)
+        A -= np.dot(b_y_u, (Y_u * s_u[:, None]))
+        A -= byY
+
+        YTSY = np.dot(Y_u.T, (Y_u * s_u[:, None]))
+        B = YTSY + YTYpR
+
+        # Binv = np.linalg.inv(B)
+        # X_new[k] = np.dot(A, Binv) 
+        X_new[k] = np.linalg.solve(B.T, A.T).T # doesn't seem to make much of a difference in terms of speed, but w/e
+
+    return X_new
+
+
+
+def factorize(P, S, num_factors, lambda_reg=1e-5, num_iterations=20, bias=False, init_std=0.01, verbose=False, dtype='float32'):
     """
     factorize a given sparse matrix using the Weighted Matrix Factorization algorithm by
     Hu, Koren and Volinsky.
@@ -80,12 +131,17 @@ def factorize(P, S, num_factors, lambda_reg=1e-5, num_iterations=20, init_std=0.
     num_iterations: the number of iterations to run the algorithm for. Each iteration consists
         of two steps, one to recompute U given V, and one to recompute V given U.
 
+    bias: learn biases for users and items.
+
     init_std: the standard deviation of the Gaussian with which V is initialized.
 
-    verbose: print a bunch of stuff during training.
+    verbose: print a bunch of stuff during training, including timing information.
 
     dtype: the dtype of the resulting factor matrices. Using single precision is recommended,
         it speeds things up a bit.
+
+    returns:
+        U, V: factor matrices. If bias=True, the last columns of the matrices contain the biases.
     """
     num_users, num_items = P.shape
 
@@ -132,21 +188,28 @@ def factorize(P, S, num_factors, lambda_reg=1e-5, num_iterations=20, init_std=0.
         print "run ALS algorithm"
         start_time = time.time()
 
+    if bias:
+        n = num_factors + 1
+        recompute_factors = _recompute_factors_biased
+    else:
+        n = num_factors
+        recompute_factors = _recompute_factors
+
     U = None # no need to initialize U, it will be overwritten anyway
-    V = np.random.randn(num_items, num_factors).astype(dtype) * init_std
+    V = np.random.randn(num_items, n).astype(dtype) * init_std
 
     for i in xrange(num_iterations):
         if verbose:
             print "  iteration %d" % i
             print "    recompute user factors U"
 
-        U = _recompute_factors(V, Sl, Dl, Il, lambda_reg, dtype=dtype)
+        U = recompute_factors(V, Sl, Dl, Il, lambda_reg, dtype=dtype)
 
         if verbose:
             print "    time since start: %.3f seconds" % (time.time() - start_time)
             print "    recompute item factors V"
 
-        V = _recompute_factors(U, STl, DTl, ITl, lambda_reg, dtype=dtype)
+        V = recompute_factors(U, STl, DTl, ITl, lambda_reg, dtype=dtype)
 
         if verbose:
             print "    time since start: %.3f seconds" % (time.time() - start_time)
