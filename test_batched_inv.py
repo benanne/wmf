883b1b4a527808bb8a1e4e25083b9c64510aeb3e
Sander Dieleman <sanderdieleman@gmail.com>
2014-09-05 14:21:24 -0400

    renamed files

--- test_batched_inv.py
+++ test_batched_inv.py
@@ -1,8 +1,8 @@
 import numpy as np
 import wmf
 import batched_inv
-import batched_inv_mp
-import batched_inv_gpu
+import solve_mp
+import solve_gpu
 
 np.random.seed(123)
 
@@ -17,8 +17,8 @@ num_iterations = 1
 batch_size = 1000
 
 # solve = batched_inv.solve_sequential
-# solve = batched_inv_mp.solve_mp
-solve = batched_inv_gpu.solve_gpu
+# solve = solve_mp.solve_mp
+solve = solve_gpu.solve_gpu
 
 
 U, V = wmf.factorize(P, S, num_factors=num_factors, lambda_reg=1e-5, num_iterations=num_iterations, init_std=0.01, verbose=True, dtype='float32',
